7641af26c8d587bac3258a28f6320b753e9da08d
Mason Jennings <mjennings@pinterest.com>
2020-05-05 13:38:09 -0700

    Validate the teams objects on next/previous names. Write the final output

--- TeamData.rb
+++ TeamData.rb
@@ -25,13 +25,48 @@ class TeamData
     end
   end
 
-  def make_primary_key
-    @team[:nameInIndex].downcase.gsub(' ', '-').gsub('–', '-').gsub(/[\(\)'\.]/, '').gsub('é', 'e')
+  def find_primary_key_override(primary_key)
+    overrides = {
+      'benetton-formula1986-2001lotus-f1-team2012-2015': 'lotus-f1',
+      'brawn-gp-formula-one-team': 'brawn-gp',
+      'caterham-f1-team': 'caterham-f1',
+      'honda-in-formula-one': 'honda-racing-f1-team',
+      'jaguar-racing-f1-team': 'jaguar-racing',
+      'lotus-racing/team-lotus': 'lotus-racing',
+      'lotus-renault-gp': 'renault-in-formula-one',
+      'manor-marussia-f1-team': 'marussia-f1',
+      'marussia-f1-team': 'marussia-f1',
+      'marussia-virgin-racing': 'virgin-racing',
+      'mercedes-gp-petronas-formula-one-team': 'mercedes-benz-in-formula-one',
+      'minardi': 'minardi-f1-team',
+      'osella': 'osella-squadra-corse',
+      'sahara-force-india': 'force-india',
+      'sauber-f1-team': 'sauber-motorsport',
+      'spyker-f1': 'spyker-f1-team',
+      'team-lotus-2010-11': 'lotus-racing',
+      'toleman': 'toleman-motorsport',
+      'force-india-f1-team': 'force-india',
+      'alfa-romeo-racing': 'alfa-romeo-in-formula-one',
+      'renault-sport-f1-team': 'renault-in-formula-one',
+      'renault-f1-team': 'renault-in-formula-one',
+    }
+
+    overrides[primary_key.to_sym] || primary_key
+  end
+
+  def make_primary_key(str)
+    if str
+      primary_key = str.downcase.gsub(' ', '-').gsub('–', '-').gsub(/[\(\)'\.]/, '').gsub('é', 'e')
+      
+      find_primary_key_override(primary_key)
+    else
+      nil
+    end
   end
 
   def extract
     data = {
-      primaryKey: make_primary_key,
+      primaryKey: make_primary_key(@team[:nameInIndex]),
       name: get_info('Full name') || @team[:nameInIndex],
       base: get_info('Base'),
       website: get_info('Website'),
@@ -47,13 +82,14 @@ class TeamData
       firstEntry: get_info('First entry'),
       engines: get_info('Engines'),
       founders: get_info('Founder'),
-      nextName: get_info('Next name'),
-      previousName: get_info('Previous name'),
+      nextName: make_primary_key(get_info('Next name')),
+      previousName: make_primary_key(get_info('Previous name')),
       notedDrivers: get_info('Noted drivers'),
       notedStaff: get_info('Noted staff'),
       teamPrincipal: get_info('Team principal'),
       technicalDirector: get_info('Technical Director'),
       results2019: get_info('2019 position'),
+      notes: nil,
     }
 
     @team.merge(data).tap{|t| t.delete(:html) }
